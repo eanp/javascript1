4d4ea8b918a4df6d2e1a42e88f67a46d53962e13
eanp <awifamz@gmail.com>
2023-02-07 10:21:59 +0700

    js3

---
+++ fetchapi.js
@@ -0,0 +1,45 @@
+import fetch from 'node-fetch';
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+
+let fetchingApi = async function(){
+    try{
+        let response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        console.log(response)
+    } catch(error) {
+        console.log('fetching error - ',error)
+    } finally{
+        console.log('api telah dipanggil')
+    }
+}
+fetchingApi()
+
+const fetchingApiPromise = fetch('htps://jsonplaceholder.typicode.com/todos/1');
+
+fetchingApiPromise.then(response=> {
+    console.log(response)
+}).catch(error=>{
+    console.log(error)
+})
+
+let janji = new Promise(
+    (res,rej) => {
+        // res('Berhasil')
+        rej('Gagal')
+    }
+)
+
+// let varpromise = new Promise( (resolve,reject)=>{})
+
+janji
+    .then((result)=>{console.log(`success - `,result)})
+    .catch((error)=>{console.log(`error - `,error)})
+
+let janjiasync = async () => {
+    let response = await janji
+    console.log(response)
+}
+
+janjiasync()
